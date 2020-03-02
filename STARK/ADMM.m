4b2ed3d290ec36d941b7f15940497a0b432248fc
jshen99 <shenoudajoseph7@gmail.com>
2020-03-02 14:02:56 -0500

    Added comments

--- STARK/ADMM.m
+++ STARK/ADMM.m
@@ -33,11 +33,8 @@ W=cell(1,K);
 
 for k=1:K
     A{k}= rand(size_dpi);
-    %W{k}= rand(size_dpi);
     W{k}=D_pi_init;
 end
-%
-
 
 %% % perform the ADMM algorithm
 
@@ -52,30 +49,7 @@ X_Gram=X*X';
 X_tilde_Gram=sparse(kron(X_Gram,speye(prod(m))));
 matr = X_tilde_Gram(PvT,PvT) + gamma*K*speye(prod(m)*prod(p));
 
-%Method (2):
-% X_tilde = sparse(kron(X,speye(prod(m))));
-% PvT=Permutation_vectors(:,2);
-% unperm_vec_1=X_tilde*Y(:);
-% vec_1=unperm_vec_1(PvT);
-% X_tilde_Gram=X_tilde*X_tilde';
-% matr = X_tilde_Gram(PvT,PvT) + gamma*K*speye(prod(m)*prod(p));
-
-%Method (3):
-% X_tilde = sparse(kron(X,speye(prod(m))));
-% PvT=Permutation_vectors(:,2);
-% Permuted_X_tilde=X_tilde(PvT,:);
-% vec_1=Permuted_X_tilde*Y(:);
-% matr = Permuted_X_tilde*Permuted_X_tilde' + gamma*K*speye(prod(m)*prod(p));
-
-%Method (4):
-% X_tilde = sparse(kron(X,speye(prod(m))));
-% P=Permutation_matrix;
-% vec_1 = P'*X_tilde*Y(:);permuting a vector is much faster than a matrix.
-% matr = P'*(X_tilde*X_tilde')*P + gamma*K*eye(prod(m)*prod(p));
-
-
 for iter=1:MaxIter
-    %    iter_admm=iter
     sum_A_W=zeros(size_dpi);
     
     for k=1:K
@@ -93,19 +67,8 @@ for iter=1:MaxIter
         % update the Lagrange multiplier equation(33)
         A{k} = A{k} - gamma*(D_pi - W{k});
     end
-    
-    
-    %    D = normcols(rearrange_Dpi_D(D_pi,[m 1],[p 1]));
-    %    Objective_value(iter)=norm(Y-D*X,'fro')^2/2+lambda* sum_trace_norm(D_pi);
-    %     if iter>1 && abs(Objective_value(iter)-Objective_value(iter-1))<tol
-    % %                 display('ADMM Converged')
-    %
-    %         break
-    %     end
-    
 end
 
 D = normcols(rearrange_Dpi_D(D_pi,m,p));
-%Objective_value=norm(Y-D*X,'fro')^2/2+lambda* sum_trace_norm(D_pi);
 
 
